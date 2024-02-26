2d44068e42f441262387e194061b12a3dbf77cc6
Mark Žgavec <mark.zgavec@gmail.com>
2024-02-26 15:53:09 +0100

    Update streamlit version

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from st_pages import show_pages, Page
 show_pages(
     [
         Page("pages/generate_upn_xml.py", "Generate UPN XML"),
-        Page("pages/convert.py", "Convert"),
-        Page("pages/analyse.py", "Analyse"),
+        #Page("pages/convert.py", "Convert"),
+        #Page("pages/analyse.py", "Analyse"),
     ]
 )
